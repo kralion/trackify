b9657b31c76a505dd488260a3260ffc43c15ae54
Brayan <joan300501@gmail.com>
2025-01-27 16:50:56 -0500

    setup for mapbox sdk

--- app/status.tsx
+++ app/status.tsx
@@ -1,5 +1,7 @@
+import { Camera, LocationPuck, MapView, MarkerView, PointAnnotation } from '@rnmapbox/maps';
 import { useLocalSearchParams } from 'expo-router';
-import { Image, ScrollView, Text, View } from 'react-native';
+import React, { useState } from 'react';
+import { ScrollView, StyleSheet, Text, TouchableOpacity, View } from 'react-native';
 
 import { Badge } from '@/components/ui/badge';
 
@@ -9,14 +11,52 @@ type Params = {
   customerName: string;
 };
 
+const styles = StyleSheet.create({
+  touchableContainer: { borderColor: 'black', borderWidth: 1.0, width: 60 },
+  touchable: {
+    backgroundColor: 'blue',
+    width: 40,
+    height: 40,
+    borderRadius: 20,
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  touchableText: {
+    color: 'white',
+    fontWeight: 'bold',
+  },
+  matchParent: { flex: 1 },
+});
+
+const INITIAL_COORDINATES: [number, number][] = [
+  [-73.99155, 40.73581],
+  [-73.99155, 40.73681],
+];
+const AnnotationContent = ({ title }: { title: string }) => (
+  <View style={styles.touchableContainer}>
+    <Text>{title}</Text>
+    <TouchableOpacity style={styles.touchable}>
+      <Text style={styles.touchableText}>Btn</Text>
+    </TouchableOpacity>
+  </View>
+);
 export default function ShippingStatusScreen() {
   const { orderDetails, destination, customerName } = useLocalSearchParams<Params>();
+  const [pointList, setPointList] = React.useState<GeoJSON.Position[]>(INITIAL_COORDINATES);
+  const [errorMsg, setErrorMsg] = useState<string | null>(null);
+
+  const [allowOverlapWithPuck, setAllowOverlapWithPuck] = React.useState<boolean>(false);
+
+  const onPressMap = (e: GeoJSON.Feature) => {
+    const geometry = e.geometry as GeoJSON.Point;
+    setPointList((pl) => [...pl, geometry.coordinates]);
+  };
 
   return (
     <ScrollView
       contentInsetAdjustmentBehavior="automatic"
-      contentContainerClassName="flex flex-col  gap-4 items-center p-4">
-      <View className="my-4 flex w-full flex-col gap-2 rounded-xl  bg-zinc-100 p-4">
+      contentContainerClassName="flex flex-col gap-4 items-center p-4">
+      <View className="my-4 flex w-full flex-col gap-2 rounded-xl bg-zinc-100 p-4">
         <View className="flex flex-row items-center gap-2">
           <Text className="text-lg font-semibold">Status:</Text>
           <Badge>
@@ -38,15 +78,46 @@ export default function ShippingStatusScreen() {
           <Text>{customerName}</Text>
         </View>
       </View>
-      <Image
-        source={{
-          uri: 'https://img.freepik.com/premium-vector/city-map-set-town-streets-with-green-line-park-river-downtown-gps-navigation-plans-abstract-transportation-urban-vector-drawing-town-small-road-maps-urban-patterns-texture_93083-1066.jpg?w=740',
-        }}
-        style={{
-          width: 350,
-          height: 500,
-        }}
-      />
+
+      {errorMsg ? (
+        <Text className="text-red-500">{errorMsg}</Text>
+      ) : location ? (
+        <View className="overflow-hidden rounded-xl" style={{ width: 350, height: 500 }}>
+          <MapView
+            style={{ flex: 1 }}
+            logoEnabled={false}
+            attributionEnabled={false}
+            scrollEnabled={true}
+            zoomEnabled={true}
+            rotateEnabled={false}
+            pitchEnabled={false}>
+            <Camera
+              defaultSettings={{
+                zoomLevel: 16,
+                centerCoordinate: pointList[0],
+              }}
+            />
+            <MarkerView coordinate={pointList[0]} allowOverlapWithPuck={allowOverlapWithPuck}>
+              <AnnotationContent title={'Punto de locacion'} />
+            </MarkerView>
+            {pointList.slice(2).map((coordinate, index) => (
+              <PointAnnotation
+                coordinate={coordinate}
+                id={`pt-ann-${index}`}
+                key={`pt-ann-${index}`}>
+                <AnnotationContent title={'Punto de locacion'} />
+              </PointAnnotation>
+            ))}
+            <LocationPuck />
+          </MapView>
+        </View>
+      ) : (
+        <View
+          style={{ width: 350, height: 500 }}
+          className="items-center justify-center bg-zinc-100">
+          <Text>Cargando mapa...</Text>
+        </View>
+      )}
     </ScrollView>
   );
 }
